1d080fcc9bae385417688eb55d216c7981665816
Anselmo Dev <anselmo.dev@gmail.com>
2020-06-23 22:37:04 -0300

    Created: Logic and validations.

--- assets/js/scripts-game.js
+++ assets/js/scripts-game.js
@@ -7,18 +7,21 @@ let scores = null,
     players = {
         p1: {
             name: null,
-            currentPoints: 0
+            currentPoints: 0,
+            launched: false
         },
         p2: {
             name: null,
-            currentPoints: 0
+            currentPoints: 0,
+            launched: false
         }
     },
     currentSideDice1 = '',
     currentSideDice2 = '',
     player1Dice = 0,
-    player2Dice = 0
-;
+    player2Dice = 0,
+    playersHistoric = [],
+    endGame = false;
 
 
 // ---------- ELEMENTS HTML ----------
@@ -66,7 +69,6 @@ function lockBtn(id, status) {
     document.getElementById(id).disabled = status;
 }
 
-
 // Show / hide boxes
 function showBoxes(id, show) {
     if (!!show) {
@@ -77,16 +79,64 @@ function showBoxes(id, show) {
     }
 }
 
+// Show points numbers on box
+function showPointsBox() {
+    document.getElementById('player1Points').innerText = players.p1.currentPoints;
+    document.getElementById('player2Points').innerText = players.p2.currentPoints;
+}
 
 // update player points
 function updatePoints(player) {
-    if (pointsType === 'inc') {
-        player === 1 ? players.p1.currentPoints++ : players.p1.currentPoints++;
+    // not winner
+    if (!player) {
+
+        setTimeout(function () {
+            alert('Empate! Jogem novamente.');
+        }, 800)
+
+    } else if (pointsType === 'inc') {
+
+        player === 1 ? players.p1.currentPoints++ : players.p2.currentPoints++;
+
     } else if (pointsType === 'dec') {
-        player === 1 ? players.p1.currentPoints-- : players.p1.currentPoints--;
+
+        player === 1 ? players.p1.currentPoints-- : players.p2.currentPoints--;
+
     }
+
+    // show points updated
+    showPointsBox();
+
+    // check winner game
+    const GET_WINNER = setPlayerWinner();
+
+    if (GET_WINNER !== 0) {
+        setTimeout(function () {
+            const WINNER_NAME = GET_WINNER === 1 ? players.p1.name : players.p2.name;
+            alert('Jogador(a) ' + WINNER_NAME + ' venceu!');
+
+            // lock game
+            lockBtn('player1Launch', true);
+            lockBtn('player2Launch', true);
+            endGame = true;
+
+        }, 1000);
+    }
+
+    // enable launch buttons and reset round
+    if (!endGame) {
+        lockBtn('player1Launch');
+        lockBtn('player2Launch');
+
+        resetRound();
+    }
+
 }
 
+// Update historic
+function updateHistoric() {
+    playersHistoric.unshift('Rodada 1: Anselmo ganhou de 5 a 3.')
+}
 
 // ---------- REGISTER PLAYERS ----------
 function registerPlayers(player1Name, player2Name) {
@@ -112,7 +162,7 @@ function setPointsType() {
 
 
 // ---------- POINTS TO PLAYERS ----------
-function setPointsPlayers(points) {
+function setPointsWinnerPlayer(points) {
     if (!points || points !== 0) {
         alert('Defina a quantidade de pontos para os jogadores!');
     } else {
@@ -128,17 +178,75 @@ function setRounds(roundNumber) {
     rounds++;
 }
 
+function resetRound(end) {
+    // reset last dice and launched
+    players.p1.launched = false;
+    players.p2.launched = false;
+    player1Dice = pointsType === 'dec' ? pointslimit : 0;
+    player2Dice = pointsType === 'dec' ? pointslimit : 0;
+    endGame = false;
+
+    // reset dice
+    if (!!end) {
+        const actualPoints = pointsType === 'dec' ? pointslimit : 0;
+
+        if (currentSideDice1 || currentSideDice2) {
+            document.getElementById('dice1').classList.remove(currentSideDice1);
+            document.getElementById('dice2').classList.remove(currentSideDice2);
+
+            currentSideDice1 = '';
+            currentSideDice2 = '';
+        }
+
+        // reset points
+        players.p1.currentPoints = actualPoints;
+        players.p2.currentPoints = actualPoints;
+
+        // show points player
+        document.getElementById('player1Points').innerText = actualPoints;
+        document.getElementById('player2Points').innerText = actualPoints;
+
+        // enabled buttons
+        lockBtn('player1Launch');
+        lockBtn('player2Launch');
+    }
+
+}
+
 
 // ---------- COMPARE RESULTS ----------
-function compareDices() {
-    if (currentSideDice1.length && currentSideDice2.length) {
+function compareDiceWinner() {
+    if (player1Dice > player2Dice) {
+        return 1;
+    } else if (player2Dice > player1Dice) {
+        return 2
+    } else {
+        return 0;
+    }
+}
 
-        if (player1Dice > player2Dice) {
-            updatePoints(1,);
+function calcResults() {
+    if (!!players.p1.launched && !!players.p2.launched) {
+        const WINNER = compareDiceWinner();
+
+        if (WINNER === 0) {
+            updatePoints();
+            return false;
+        }
+
+        // remove points from loser
+        if (pointsType === 'dec') {
+            WINNER === 1 ? updatePoints(2) : updatePoints(1);
+        }
+
+        // add points to winner
+        else if (pointsType === 'inc') {
+            WINNER === 1 ? updatePoints(1) : updatePoints(2);
         }
 
     }
 }
+
 
 // ---------- NEW GAME ----------
 function setNewGame() {
@@ -171,6 +279,11 @@ function registerLastDice(number, randomNumber) {
         setTimeout(function () {
             DICE.classList.add(showDiceFace);
             currentSideDice1 = showDiceFace;
+            players.p1.launched = true;
+            player1Dice = randomNumber;
+
+            // check winner
+            calcResults();
         }, 35);
     } else {
         if (currentSideDice2) {
@@ -181,8 +294,14 @@ function registerLastDice(number, randomNumber) {
         setTimeout(function () {
             DICE.classList.add(showDiceFace);
             currentSideDice2 = showDiceFace;
+            players.p2.launched = true;
+            player2Dice = randomNumber;
+
+            // check winner
+            calcResults();
         }, 35);
     }
+
 }
 
 function raffledNumberOnDice(number) {
@@ -208,7 +327,7 @@ function diceAnimation(diceNumber) {
     }, 2000);
 }
 
-function playDices(player) {
+function playDice(player) {
 
     if (player === 1) {
         diceAnimation(1);
@@ -219,7 +338,23 @@ function playDices(player) {
 
 
 // ---------- PLAYER WINNER ----------
-function setPlayerWinner(player) {
+function setPlayerWinner() {
+    if (pointsType === 'inc' && players.p1.currentPoints === pointslimit) {
+        return 1;
+    } else if (pointsType === 'inc' && players.p2.currentPoints === pointslimit) {
+        return 2;
+    } else if (pointsType === 'dec' && players.p1.currentPoints === 0) {
+        return 2;
+    } else if (pointsType === 'dec' && players.p2.currentPoints === 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+// ---------- PLAYER WINNER TO SCORE ----------
+function setPlayerWinnerScore(player) {
     if (player === 1) {
 
     } else if (player === 2) {
@@ -237,22 +372,23 @@ BTN_START_GAME.addEventListener('click', function () {
     const p1Name = PLAYER1_NAME.value;
     const p2Name = PLAYER2_NAME.value;
     const ptType = POINTS_TYPE.value;
-    const ptLimit = POINTS_LIMIT.value;
+    const ptLimit = Number(POINTS_LIMIT.value);
 
     if (p1Name.length < 3 || p2Name.length < 3) {
 
         alert('É obrigatório o nome dos jogadores! Mínimo de 3 caracteres.');
 
-    } else if (ptType === 'none' || !ptLimit.length || ptLimit === '0') {
-        alert('É obrigatório definir o Tipo de Pontuação e Limite de Pontos!');
+    } else if (ptType === 'none' || ptLimit <= 1) {
+        alert('É obrigatório definir o Tipo de Pontuação e Limite de Pontos de pelo menos (2)!');
     } else {
 
         players.p1.name = p1Name;
         players.p2.name = p2Name;
+        pointslimit = Number(ptLimit);
 
         if (POINTS_TYPE.value === 'dec') {
-            players.p1.currentPoints = Number(POINTS_LIMIT.value);
-            players.p2.currentPoints = Number(POINTS_LIMIT.value);
+            players.p1.currentPoints = POINTS_LIMIT.value;
+            players.p2.currentPoints = POINTS_LIMIT.value;
         }
 
         // go to box game
@@ -283,35 +419,54 @@ BTN_EXIT_SCORES.addEventListener('click', function () {
 
 BTN_PL1_LAUNCH.addEventListener('click', function () {
     lockBtn('player1Launch', true);
-    playDices(1);
+    playDice(1);
 });
 
 BTN_PL2_LAUNCH.addEventListener('click', function () {
     lockBtn('player2Launch', true);
-    playDices(2);
+    playDice(2);
 });
 
 BTN_RESTART_GAME.addEventListener('click', function () {
     const QUESTION = confirm(
-        'Reiniciar o Jogo, definirá o ganhador com base nos pontos atuais! Deseja continuar?'
+        'Deseja Reiniciar o Jogo? A partida atual será perdida.'
     );
 
     // Check winner and reset game
     if (!!QUESTION) {
-        console.log('Restart Game!');
-        // setNewGame();
+        resetRound(true);
     }
 });
 
 BTN_END_GAME.addEventListener('click', function () {
     const QUESTION = confirm(
-        'Finalizar o Jogo, definirá o ganhador com base nos pontos atuais! Deseja continuar?'
+        'Deseja Realmente Finalizar o Jogo?'
     );
 
-    // Check winner, reset game and open Register Box
+    // Check winner and reset game
     if (!!QUESTION) {
-        console.log('End Game!');
-        // setNewGame();
+        resetRound(true);
+
+        // clean form
+        PLAYER1_NAME.value = '';
+        PLAYER2_NAME.value = '';
+        POINTS_TYPE.value = 'none';
+        POINTS_LIMIT.value = 10;
+
+        // clean data
+        players.p1.name = null;
+        players.p2.name = null;
+        pointslimit = 0;
+        players.p1.currentPoints = 0;
+        players.p2.currentPoints = 0;
+
+        // clean players name
+        document.getElementById('playe1Name').innerText = '';
+        document.getElementById('playe2Name').innerText = '';
+
+        // go to box register
+        showBoxes('game');
+        showBoxes('register', true);
     }
 
 });
